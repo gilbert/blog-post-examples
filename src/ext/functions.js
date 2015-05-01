f28aa5f60a83583cdc0864fe02f7a30a7fd37a34
Gilbert <gilbertbgarza@gmail.com>
2015-05-01 08:22:00 -0500

    Add documentation from gist

--- src/ext/functions.js
+++ src/ext/functions.js
@@ -1,4 +1,11 @@
-
+// Partially apply arguments to a function. Useful for binding
+// specific data to an event handler.
+// Example:
+//
+//   var add = function (x,y) { return x + y; }
+//   var add5 = add.papp(5)
+//   add5(7) //=> 11
+//
 Function.prototype.papp = function () {
   var slice = Array.prototype.slice
   var fn = this
@@ -7,3 +14,6 @@ Function.prototype.papp = function () {
     fn.apply(this, args.concat(slice.call(arguments)))
   }
 }
+
+// For more useful function extensions, see:
+// https://gist.github.com/mindeavor/13d4593b00b4ee7cea33
