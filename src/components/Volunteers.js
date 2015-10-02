472ebfaa80c6fa63d1d9fbfd57b91ba5bb37face
Gilbert <gilbertbgarza@gmail.com>
2015-10-02 09:29:54 -0500

    Add placeholder for part 2

---
+++ src/components/Volunteers.js
@@ -0,0 +1,10 @@
+window.Volunteers = {}
+
+Volunteers.controller = function (attrs) {
+  var ctrl = this
+  // TODO: Controller code goes here
+}
+
+Volunteers.view = function (ctrl, attrs) {
+  // TODO: View code goes here
+}
