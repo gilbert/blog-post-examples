0f6d82c89664f9a4d85242f22c28d993ebaf2a39
Gilbert <gilbertbgarza@gmail.com>
2018-12-06 16:55:16 -0600

    Solution for Mithril Part 1

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,12 +1,47 @@
 
 window.EntryForm = function () {
-  // TODO: State goes here
+  var entry = Entry.vm()
+
+  function add () {
+    entry.volunteers.push( Entry.volunteerVM() )
+  }
+  function remove (idx) {
+    entry.volunteers.splice(idx, 1)
+  }
+  function submit () {
+    Entry.create(entry)
+    m.route.set('/')
+  }
 
   return {
     view() {
+      var showRemove = (entry.volunteers.length >= 2)
       return m('.entry-form', [
         m('h1', "New Entry"),
-        // TODO
+        m('h3', "Please enter each volunteer's contact information:"),
+
+        entry.volunteers.map(function(volunteer, idx) {
+          return m('fieldset', [
+            m('legend', "Volunteer #" + (idx+1)),
+            m('label', "Name:"),
+            m('input[type=text]', {
+              value: volunteer.name,
+              onchange(e) {
+                volunteer.name = e.target.value
+              }
+            }),
+            m('br'),
+            m('label', "Email:"),
+            m('input[type=text]', { value: volunteer.email }),
+
+            showRemove &&
+            m('button', { onclick() { remove(idx) } }, 'remove')
+          ])
+        }),
+
+        m('button', { onclick: add }, 'Add another volunteer'),
+        m('br'),
+        m('button', { onclick: submit }, 'Submit'),
       ])
     }
   }
--- src/components/EntryList.js
+++ src/components/EntryList.js
@@ -1,6 +1,28 @@
 
 window.EntryList = {
   view() {
-    return m('h2', "TODO: REPLACE ME!")
+    return m('.entry-list', [
+      m('h1', "All Entries"),
+
+      m('a[href=/entries/new]', { oncreate: m.route.link }, "Add New Entry"),
+
+      Entry.all().map( entryView )
+    ])
   }
+}
+
+function entryView (entry) {
+  var date = new Date(entry.enteredAt)
+
+  return m('.entry', [
+    m('label', "Entered at: ", date.toString()),
+    m('ul', entry.volunteers.map( volunteerView ))
+  ])
+}
+
+function volunteerView (volunteer) {
+  return m('li.volunteer', [
+    m('label', volunteer.name),
+    m('label', "(" + volunteer.email + ")")
+  ])
 }
